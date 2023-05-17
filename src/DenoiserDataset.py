30dc808b0ba0e69831bd0e10f589c8ba3211e4d7
Alexis JAPAS <alexis.japas.dev@pm.me>
2023-05-17 19:24:10 +0200

    transform and noise added to the dataset

--- src/DenoiserDataset.py
+++ src/DenoiserDataset.py
@@ -1,18 +1,26 @@
 from torch.utils.data import Dataset
+from torchvision.io.image import read_image
 import os
 
 
 class DenoiserDataset(Dataset):
-    def __init__(self, data_path, transform=None):
+    def __init__(self, data_path, noise, transform=None):
         self.paths = [
             os.path.join(data_path, p)
             for p in os.listdir(data_path)
             if os.path.isfile(os.path.join(data_path, p))
         ]
+        self.noise = noise
         self.transform = transform
 
     def __getitem__(self, idx):
-        pass  # TODO
+        hr = read_image(self.paths(idx))
+        if self.transform:
+            hr = self.transform(hr)
+
+        inlr = self.noise(hr)
+
+        return inlr, hr
 
     def __len__(self):
         return len(self.paths)
